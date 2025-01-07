0f326a4f4a8f42b5958d07dd9dd586c137082fd3
Shravan250 <shravanbobade25@gmail.com>
2025-01-07 23:55:58 +0530

    added ass 5 patch 1

--- Ass5/routerdom/src/components/AboutUs.jsx
+++ Ass5/routerdom/src/components/AboutUs.jsx
@@ -1,44 +1,44 @@
-import React from 'react'
-import "./AboutUs.css"
+import React from "react";
+import "./AboutUs.css";
 
 const AboutUs = () => {
   return (
     <div>
       <header>
-      <h1>About Me</h1>
-    </header>
+        <h1>About Me</h1>
+      </header>
 
-    <div className="about">
-      <div className="intro">
-        <img
-          src="https://cdn.pixabay.com/photo/2023/01/23/00/45/cat-7737618_1280.jpg"
-          alt="Profile picture"
-        />
-        <div className="text">
-          <h2>Hi, I'm Atharva Dhawan</h2>
-          <p>
-            I'm a passionate web developer with a love for creating intuitive
-            and dynamic web experiences. With experience in front-end and
-            back-end technologies, I strive to build products that are
-            user-friendly and efficient. I enjoy solving problems and
-            continuously learning new skills.
-          </p>
+      <div className="about">
+        <div className="intro">
+          <img
+            src="https://cdn.pixabay.com/photo/2023/01/23/00/45/cat-7737618_1280.jpg"
+            alt="Profile picture"
+          />
+          <div className="text">
+            <h2>Hi, I'm Shravan Bobade</h2>
+            <p>
+              I'm a passionate web developer with a love for creating intuitive
+              and dynamic web experiences. With experience in front-end and
+              back-end technologies, I strive to build products that are
+              user-friendly and efficient. I enjoy solving problems and
+              continuously learning new skills.
+            </p>
+          </div>
+        </div>
+
+        <div className="skills">
+          <h3>My Skills</h3>
+          <ul>
+            <li>HTML5, CSS3, JavaScript</li>
+            <li>React</li>
+            <li>Node.js, Express</li>
+            <li>MongoDB</li>
+            <li>Git, GitHub, Version Control</li>
+          </ul>
         </div>
       </div>
-
-      <div className="skills">
-        <h3>My Skills</h3>
-        <ul>
-          <li>HTML5, CSS3, JavaScript</li>
-          <li>React</li>
-          <li>Node.js, Express</li>
-          <li>MongoDB</li>
-          <li>Git, GitHub, Version Control</li>
-        </ul>
-      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
